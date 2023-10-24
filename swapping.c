10ed68e9175d1e8ecc6b0d0bb6e3c8dc3c6dc120
sarafkushagra <147098460+sarafkushagra@users.noreply.github.com>
2023-10-24 18:33:56 +0530

    Create swapping.c

---
+++ swapping.c
@@ -0,0 +1,16 @@
+//Swapping
+#include<stdio.h>
+int main()
+(
+int a,b;
+printf("Enter first number: ");
+scanf("%d",&a);
+printf("Enter second number: ");
+scanf("%d", &b);
+a=a+b;
+b=a-b;
+a=a-b
+printf("A_SWAPPED=%d", a);
+printf("B_SWAPPED=%d", b);
+return 0;
+}
